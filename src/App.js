fafe5b553ffa0a03c47a5d94721a73b415cba867
nsihor <nsihor@gmail.com>
2023-11-12 03:29:45 +0200

    add routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import {Route, Routes} from "react-router-dom";
+import Layout from "./components/Layout/Layout";
+import MainPage from "./pages/Main/MainPage";
+import CatalogPage from "./pages/Catalog/CatalogPage";
+import FavoritesPage from "./pages/Favorites/FavoritesPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<MainPage  />} />
+          <Route path="/catalog" element={<CatalogPage />} />
+          <Route path="/favorites" element={<FavoritesPage />} />
+        </Route>
+        <Route path="*" element={<MainPage />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Catalog/CatalogPage.jsx
@@ -0,0 +1,3 @@
+const CatalogPage = () => {}
+
+export default CatalogPage
